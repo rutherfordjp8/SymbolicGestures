8dd46eb27290f20a5c205fcfc1b44d91d2869b3b
Patrick Rutherford <jpatrickrutherford@gmail.com>
2017-09-02 16:33:42 -0700

    Added css-loader and style-loader

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -19,6 +19,17 @@ const config = {
             }
           }
         ]
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader'
+      }, {
+        test: /\.css$/,
+        loader: 'css-loader',
+        query: {
+          modules: true,
+          localIdentName: '[name]__[local]___[hash:base64:5]'
+        }
       }
     ]
   }
