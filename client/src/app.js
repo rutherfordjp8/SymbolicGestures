c8171e6654d6d0596d030192d25462557775209d
sean0923 <iamseanhong@gmail.com>
2017-09-02 16:56:23 -0700

    Waiting for Pat pull req

--- client/src/app.js
+++ client/src/app.js
@@ -1,4 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>Hello World from React</h1>, document.getElementById('root'));
+import TableAndDrawer from './components/TableAndDrawer.jsx';
+
+const fakeApplicationsGenerator = require('./fakeApplcationsGenerator.js');
+
+let fakeApplications = fakeApplicationsGenerator(15);
+
+let seanStyleBox = './../styles/seanStyleBox.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      applications: fakeApplications
+    };
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="box_w100per_h70px" />
+
+        {/* <div className="box_94per_3perMg"> */}
+        <div className={seanStyleBox.box_94per_3perMg}>
+          <div className="PatrickStatusBar">
+            <h3>Patrick Status Bar</h3>
+          </div>
+        </div>
+
+        <div className="box_94per_3perMg">
+          <TableAndDrawer
+            applications={this.state.applications}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
