6ee0a1641772e421d15caddef4d2fa6abd6eed2d
sean0923 <iamseanhong@gmail.com>
2017-09-05 16:14:28 -0700

    Import axios

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios'
+
 import AppDrawer from './components/drawer/appDrawer.jsx'
 
 import DrawerAndApplicationTable from './components/DrawerAndApplicationTable.jsx';
