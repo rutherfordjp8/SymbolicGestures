23e567044a14391c47d3b338517bb35266604ee1
Jonathan Kim <jykim16@gmail.com>
2017-09-09 13:56:28 -0700

    webpack for prod mode

---
+++ webpack.dev.config.babel.js
@@ -0,0 +1,44 @@
+import webpack from 'webpack';
+import path from 'path';
+const config = {
+  plugins: [
+    new webpack.DefinePlugin({
+     'process.env': {
+       'NODE_ENV': JSON.stringify('production')
+     }
+   })
+  ],
+  entry: './client/src/app.jsx',
+  output: {
+    path: path.join(__dirname, 'public/dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.(js|jsx)$/,
+        include: path.join(__dirname, 'client/src'),
+        exclude: ['node_modules'],
+        use: [
+          { loader: 'babel-loader',
+            options: {
+              presets: ['react', 'es2015']
+            }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader'
+      }, {
+        test: /\.css$/,
+        loader: 'css-loader',
+        query: {
+          modules: true,
+          localIdentName: '[name]__[local]___[hash:base64:5]'
+        }
+      }
+    ]
+  }
+};
+
+export default config;
