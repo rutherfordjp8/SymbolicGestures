79b61d244344777ab361f67c44ecb2018653619d
sean0923 <iamseanhong@gmail.com>
2017-09-09 20:40:44 -0700

    Clean up code before push

--- client/src/components/drawer/AppDrawerInfoDropDown.jsx
+++ client/src/components/drawer/AppDrawerInfoDropDown.jsx
@@ -2,22 +2,6 @@ import React from 'react';
 import { Dropdown } from 'semantic-ui-react';
 import axios from 'axios';
 
-let fakestages_settings = [
-  { name: 'Applied', backgroundColor: '#FFC107', textColor: 'black' },
-  { name: 'Phone Screen', backgroundColor: '#2196F3', textColor: 'white' },
-  { name: 'OFFER', backgroundColor: '#009688', textColor: 'white' },
-  { name: 'Denied', backgroundColor: '#F44336', textColor: 'white' },
-  { name: 'On Site', backgroundColor: '#F24326', textColor: 'white' }
-];
-let fakeStageNameToColorHash = {};
-fakestages_settings.forEach((setting) => {
-  fakeStageNameToColorHash[setting.name] = {
-    backgroundColor: setting.backgroundColor,
-    color: setting.textColor,
-  };
-});
-
-
 class AppDrawerInfoDropDown extends React.Component {
   constructor(props) {
     super(props);
