c59579408ef201a1aa22a91471937c9e3a05112a
Patrick Rutherford <jpatrickrutherford@gmail.com>
2017-09-13 16:28:37 -0700

    Took out add and edit stage bar features

--- client/src/components/stageBar/StageBarEntry.jsx
+++ client/src/components/stageBar/StageBarEntry.jsx
@@ -9,44 +9,12 @@ const StageBarEntry = (props) => {
     'background': props.stage.backgroundColor,
     'flex': `${props.flexSize}`
   };
-
-  let handleMouseOver = function(el) {
-    if (el.target.firstChild.className) {
-      el.target.firstChild.firstChild.style.visibility = 'visible';
-    } else {
-      el.target.firstChild.style.visibility = 'visible';
-    }
-    // el.target.style['flex-grow'] = style.flex + 1;
-
-    // if (settingsName === 'stageBar') {
-    //   el.target.firstChild.firstChild.style.visibility = 'visible';
-    // } else {
-    //   el.target.firstChild.style.visibility = 'visible';
-    //   el.target.style['flex-grow'] = style.flex + 1;
-    // }
-    // el.target.firstChild.firstChild.style.display = 'block';
-    // el.target.style.flex = style.flex + 1;
+  if(props.stage.name === 'Denied') {
+    style.flex = 1;
   }
-  let handleMouseOut = function(el) {
-    // console.log('MO', el.target.parentNode.className.substring(0,8));
-    // if (el.target.parentNode.className.substring(0,8) === 'stageBar') {
-    //   return;
-    // } else  {
-      // el.target.firstChild.firstChild.style.visibility = 'hidden';
-    // }
-    // el.target.firstChild.firstChild.style.display = 'none';
-    // el.target.style.width = 'initial';
 
-    // el.target.style.flex = style.flex;
-
-
-  }
   return (
     <li style={style}
-      onClick={props.handleOnClick}
-      // onChange={console.log('change')}
-      onMouseOver={handleMouseOver}
-      onMouseOut={handleMouseOut}
     >
       <div className={styles.settings}>
         <Icon
