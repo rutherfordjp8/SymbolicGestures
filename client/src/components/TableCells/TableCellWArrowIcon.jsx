bb9fef36977b8b7d530e4a8a7c845a344ce2ca1b
sean0923 <iamseanhong@gmail.com>
2017-09-14 19:40:52 -0700

    Make TableCellWArroIcon.jsx

---
+++ client/src/components/TableCells/TableCellWArrowIcon.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { Table, Segment, Button, Icon, Checkbox } from 'semantic-ui-react';
+
+class TableCellWArrowIcon extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      something: '',
+    };
+  }
+
+  render() {
+    let openDrawerWhenOneAppClick = this.props.openDrawerWhenOneAppClick;
+    let application = this.props.application;
+    let idx = this.props.idx;
+    if (idx === this.props.selectedAppIdx) {
+      return (
+        <Table.Cell
+          onClick={(e) => (openDrawerWhenOneAppClick(application, idx, e))}
+          style={{ cursor: 'pointer' }}
+          collapsing
+        >
+          <Icon style={{ color: 'black' }} name="chevron right" />
+          <Icon style={{ color: 'black' }} name="chevron right" />
+        </Table.Cell>
+      );
+    }
+    return (
+      <Table.Cell
+        onClick={(e) => (openDrawerWhenOneAppClick(application, idx, e))}
+        style={{ cursor: 'pointer' }}
+        collapsing
+      >
+        <Icon style={{ color: 'black' }} name="chevron left" />
+      </Table.Cell>
+    );
+  }
+}
+
+export default TableCellWArrowIcon;
