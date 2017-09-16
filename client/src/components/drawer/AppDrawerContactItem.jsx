889631e4e57043b65b318eb23b7170b88602d600
sean0923 <iamseanhong@gmail.com>
2017-09-15 21:01:44 -0700

    Pull from Jerry and Clean up code

--- client/src/components/drawer/AppDrawerContactItem.jsx
+++ client/src/components/drawer/AppDrawerContactItem.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 
-import { Card } from 'semantic-ui-react'
-import { Icon } from 'semantic-ui-react'
+import { Card, Icon } from 'semantic-ui-react';
 
 import styles from '../../../styles/jerryStyleBox.css';
 
@@ -21,37 +20,38 @@ class AppDrawerContactItem extends React.Component {
     this.setState({
       controlledDate: date,
     });
-  };
+  }
 
   handleDeleteContact(event) {
-   let route = `/api/deleteContact/${this.props.contact.id}`
-   axios.post(route)
-     .then(this.props.getApplicationsFromDB())
+    let route = `/api/deleteContact/${this.props.contact.id}`
+    axios.post(route)
+      .then(this.props.getApplicationsFromDB())
   }
+
   handleEdit(event) {
-   console.log('edit the contact with this id:' , this.props.contact.id)
+    console.log('edit the contact with this id:' , this.props.contact.id)
   }
 
   render() {
     return (
       <div className={styles.contact}>
-          <Card>
-            <Card.Content>
-              <Card.Header>
-                <Icon link name='close' onClick={this.handleDeleteContact}/>
-              </Card.Header>
-              <Card.Header>{this.props.contact.name}</Card.Header>
+        <Card>
+          <Card.Content>
+            <Card.Header>
+              <Icon link name='close' onClick={this.handleDeleteContact}/>
+            </Card.Header>
+            <Card.Header>{this.props.contact.name}</Card.Header>
 
-              <Card.Meta>{this.props.contact.role}</Card.Meta>
-              <Card.Description>{this.props.contact.email}</Card.Description>
-              <Card.Description>{this.props.contact.phone}</Card.Description>
-              <Card.Description>{"Last contact: " + this.props.contact.last_contact_date.slice(0,10)}</Card.Description>
-              {/* <DatePicker
-                 selected={this.state.controlledDate}
-                 onChange={this.handleChange}
-              /> */}
-            </Card.Content>
-          </Card>
+            <Card.Meta>{this.props.contact.role}</Card.Meta>
+            <Card.Description>{this.props.contact.email}</Card.Description>
+            <Card.Description>{this.props.contact.phone}</Card.Description>
+            <Card.Description>{"Last contact: " + this.props.contact.last_contact_date.slice(0,10)}</Card.Description> 
+            {/* <DatePicker
+                selected={this.state.controlledDate}
+                onChange={this.handleChange}
+            /> */}
+          </Card.Content>
+        </Card>
       </div>
     );
   }
