341c8974b50453327845e7f9ac97348294260ce1
jerrysummer <reason2be@gmail.com>
2017-09-16 18:05:23 -0700

    Datepick icon renders under date

--- client/src/components/drawer/AppDrawerContactItem.jsx
+++ client/src/components/drawer/AppDrawerContactItem.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import axios from 'axios';
 
+<<<<<<< HEAD
 import { Card, Icon } from 'semantic-ui-react';
+=======
+import { Card } from 'semantic-ui-react'
+import { Icon } from 'semantic-ui-react'
+import DatePicker from 'material-ui/DatePicker';
+>>>>>>> Datepick icon renders under date
 
 import styles from '../../../styles/jerryStyleBox.css';
 
@@ -14,6 +20,7 @@ class AppDrawerContactItem extends React.Component {
     this.handleDeleteContact = this.handleDeleteContact.bind(this);
     this.handleEdit = this.handleEdit.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.openDatePicker = this.openDatePicker.bind(this);
   }
 
   handleChange(event, date) {
@@ -31,27 +38,37 @@ class AppDrawerContactItem extends React.Component {
   handleEdit(event) {
     console.log('edit the contact with this id:' , this.props.contact.id)
   }
+  openDatePicker(){
+    this.refs.datepicker.openDialog()
+  }
 
   render() {
     return (
       <div className={styles.contact}>
-        <Card>
-          <Card.Content>
-            <Card.Header>
-              <Icon link name='close' onClick={this.handleDeleteContact}/>
-            </Card.Header>
-            <Card.Header>{this.props.contact.name}</Card.Header>
+          <Card>
+            <Card.Content>
+              <Card.Header>
+                <Icon link name='close' onClick={this.handleDeleteContact}/>
+              </Card.Header>
+              <Card.Header>{this.props.contact.name}</Card.Header>
 
-            <Card.Meta>{this.props.contact.role}</Card.Meta>
-            <Card.Description>{this.props.contact.email}</Card.Description>
-            <Card.Description>{this.props.contact.phone}</Card.Description>
-            <Card.Description>{"Last contact: " + this.props.contact.last_contact_date.slice(0,10)}</Card.Description> 
-            {/* <DatePicker
-                selected={this.state.controlledDate}
+              <Card.Meta>{this.props.contact.role}</Card.Meta>
+              <Card.Description>{this.props.contact.email}</Card.Description>
+              <Card.Description>{this.props.contact.phone}</Card.Description>
+              <Card.Description>{"Last contact: " + this.props.contact.last_contact_date.slice(0,10)}</Card.Description>
+
+              <Icon link name='calendar' onClick={this.openDatePicker}/>
+
+              <DatePicker
+                ref='datepicker'
+                style={{display: 'none'}}
+                hintText="Controlled Date Input"
+                value={this.state.controlledDate}
                 onChange={this.handleChange}
-            /> */}
-          </Card.Content>
-        </Card>
+              />
+
+            </Card.Content>
+          </Card>
       </div>
     );
   }
