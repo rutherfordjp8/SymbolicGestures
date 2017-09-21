0509ec7ad900f7b2d3f1e68fdf3daea7856cf737
sean0923 <iamseanhong@gmail.com>
2017-09-21 12:02:51 -0700

    Formatted SocialGraph.jsx

--- client/src/components/Analytics/SocialGraph.jsx
+++ client/src/components/Analytics/SocialGraph.jsx
@@ -3,64 +3,63 @@ import styles from './../../../styles/socialGraphStyles.css'
 import PropTypes from 'prop-types'
 import C3Chart from 'react-c3js'
 
-let SocialGraph = (props) => {
+const SocialGraph = (props) => {
   let graphHeight = window.innerHeight - 180;
-//   console.log(props.socialGraphData)
-  return(
-      <C3Chart
-        size={{
-          height: graphHeight
-        }}
-        padding={{
-          left: 140
-        }}
-        color={{
-          pattern: ['#0da17d']
-        }}
-        data={{
-          json: props.socialGraphData,
-          keys: {
-            x: 'name',
-            value: ['percentage']
-          },
-          type: 'bar',
-          labels: {
-              format: function (v, id, i, j) { return v+"%"; }
+  return (
+    <C3Chart
+      size={{
+        height: graphHeight
+      }}
+      padding={{
+        left: 140
+      }}
+      color={{
+        pattern: ['#0da17d']
+      }}
+      data={{
+        json: props.socialGraphData,
+        keys: {
+          x: 'name',
+          value: ['percentage']
+        },
+        type: 'bar',
+        labels: {
+          format: function (v, id, i, j) { return v + "%"; }
+        }
+      }}
+      axis={{
+        rotated: true,
+        x: {
+          type: 'categorized',
+          //for column label
+          // label: {
+          //   text: '',
+          //   position: 'outer-middle'
+          // },
+          tick: {
+            multiline: false
           }
-        }}
-        axis={{
-          rotated: true,
-          x: {
-            type:'categorized',
-            //for column label
-            // label: {
-            //   text: '',
-            //   position: 'outer-middle'
-            // },
-            tick: {
-              multiline: false
-            }
-          },
-          y: {
-            show: false
-          }
-        }}
-        grid={{
-          x: {
-            show: true
-          }
-        }}
-        title={{
-          text: `My Organization's Individual Highest Salary Offered`
-        }}
-        legend={{hide: ['percentage']}}
-        tooltip={{
-          format: {
-            name: (name, ratio, id, index) => { return 'Count:'; },
-            value: (value, ratio, id, index) => {  return props.socialGraphData[index].count; }
-          }
-        }}
-      />
+        },
+        y: {
+          show: false
+        }
+      }}
+      grid={{
+        x: {
+          show: true
+        }
+      }}
+      title={{
+        text: `My Organization's Individual Highest Salary Offered`
+      }}
+      legend={{ hide: ['percentage'] }}
+      tooltip={{
+        format: {
+          name: (name, ratio, id, index) => { return 'Count:'; },
+          value: (value, ratio, id, index) => { return props.socialGraphData[index].count; }
+        }
+      }}
+    />
   )
 }
 
